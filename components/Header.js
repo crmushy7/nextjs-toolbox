46556fc01024d61c5aeac31c707a3814ad82af09
crmushy7 <112522385+crmushy7@users.noreply.github.com>
2022-10-12 02:19:38 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,35 +1,6 @@
 export default function Header({ title }) {
   return (
     <>
-    <!DOCTYPE html>
-<html lang="en">
-  <head>
-
-
-
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width" />
-    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-    <title>{title}</title>
-  </head>
-  <body>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3798212441478713"
-     crossorigin="anonymous"></script>
-<!-- GEEMUSHY -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3798212441478713"
-     data-ad-slot="6589693262"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
- 
-  </body>
-</html>
-
-    
       <h1 className="title">{title}</h1>
       <p>
        You can see the history of English Premium League {" "}
