2c8546a8e711d9719e0c9f824c7da49b1d62ad87
crmushy7 <112522385+crmushy7@users.noreply.github.com>
2022-10-11 23:57:16 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -101,21 +101,6 @@ Documentary film Alegria do Povo (1963)
 
         </p>
   
-    <p>
-       You can also learn about Engish Premium League(EPL){" "}
-        <a href="https://www.footballhistory.org/league/premier-league.html">EPL</a> {" "}
-        <a href="https://www.footballhistory.org/league/premier-league.html">
-        
-          You can also learn about Spanish Premium League(Laliga){" "}
-        <a href="https://www.footballhistory.org/league/premier-league.html">Laliga</a> {" "}
-        <a href="https://www.footballhistory.org/league/premier-league.html">
-        </a>
-      
-      </p>
-
-   
-  
-  
   
         <FeedbackForm />
       </main>
