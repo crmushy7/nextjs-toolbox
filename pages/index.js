f801b8e2abeec539d8aaaac4ef07b40b4e1aedde
crmushy7 <112522385+crmushy7@users.noreply.github.com>
2022-10-12 01:17:14 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,22 @@ export default function Home() {
         <title>FOOTBALL HISTORY</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+    <body>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3798212441478713"
+     crossorigin="anonymous"></script>
+<!-- CRMUSHY -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3798212441478713"
+     data-ad-slot="4198746915"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+  
+  </body>
+    
 
       <main>
         <Header title="THE HISTORY OF FOOTBALL" />
