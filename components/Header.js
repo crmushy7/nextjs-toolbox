e8d1595fdc23228e1de77a2579db74887e0897d7
crmushy7 <112522385+crmushy7@users.noreply.github.com>
2022-10-12 00:01:39 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,22 @@ export default function Header({ title }) {
     <>
       <h1 className="title">{title}</h1>
       <p>
+  
+    <p>
+       You can also learn about Engish Premium League(EPL){" "}
+        <a href="https://www.footballhistory.org/league/premier-league.html">EPL</a> {" "}
+        <a href="https://www.footballhistory.org/league/premier-league.html"\n>
+        
+          You can also learn about Spanish Premium League(Laliga){" "}
+        <a href="https://www.footballhistory.org/league/premier-league.html">Laliga</a> {" "}
+        <a href="https://www.footballhistory.org/league/premier-league.html"\n>
+        </a>
+      
+      </p>
+
+   
+  
+  
       </p>
 
       <p>
